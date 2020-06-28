d9906bef0807151bf7f9de8dada2a952eb680e51
Jhonatan Acero <stiddark@gmail.com>
2020-06-28 18:43:22 -0500

    routing, login, mejoras

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { CarComponent } from './components/car/car.component';
+import { AdministradorComponent } from './components/administrador/administrador.component';
+import { AuthGuard } from './services/auth.guard';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'productos', component: ProductosComponent},
+  {path: 'car', component: CarComponent, canActivate: [AuthGuard]},
+  {path: 'administrar', component: AdministradorComponent, canActivate: [AuthGuard]},
+  { path: '**', pathMatch: 'full', redirectTo: 'home' },
+  { path: '', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
